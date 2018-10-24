336abe16f5db65df898fc4b7ecfba84e02b06dc4
Aayush Sinha <aayush.aryan@me.com>
2018-10-24 11:51:45 +0530

    Add Library to GCE Endpoints

--- backend/src/main/java/com/aayushsinha/jokes/JokesLib.java
+++ backend/src/main/java/com/aayushsinha/jokes/JokesLib.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.aayushsinha.jokesapplib.javalib;
 
 /**
  * An endpoint class we are exposing
@@ -21,22 +22,10 @@ import com.google.api.server.spi.config.ApiNamespace;
 
 public class JokesLib {
 
-  static String[] jokes = {
-            "This is Joke 1",
-            "This is a funny Joke #2",
-            "This is a very funny Joke number 3",
-            "This is superfunny Joke Number 4",
-            "This is another Joke",
-            "Ha Ha Ha",
-            "OMG, This is soo funny",
-            "This is soo funny, that this will definitely offend someone"
-    };
-
     @ApiMethod(name = "tellJoke")
     public ResponseJoke tellJoke() {
-        Random rndm = new Random();
         ResponseJoke res = new ResponseJoke();
-        res.setJoke(jokes[rndm.nextInt(jokes.length)]);
+        res.setJoke(JavaLib.tellJoke());
         return res;
     }
 
